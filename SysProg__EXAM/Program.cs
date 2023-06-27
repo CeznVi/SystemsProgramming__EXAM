664e76da57177455e419dec93a7a1c7ec9295c41
Viktor Velychko <Cezn.Vi@gmail.com>
2023-06-27 21:48:02 +0300

    W I P

---
+++ SysProg__EXAM/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace SysProg__EXAM
+{
+    internal class Program
+    {
+
+        /// <summary>
+        /// Переменная для хранения пароля который успешно подобран
+        /// </summary>
+        private static string realPassword = "";
+
+        /// <summary>
+        /// Строка допустимых символов которые могут содержаться в пароле
+        /// </summary>
+        private const string allPossibleSymbol = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*_";
+
+
+
+        static void Main(string[] args)
+        {
+
+            
+        }
+    }
+}
