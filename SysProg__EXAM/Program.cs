a77d005c2f59c9e013c13e52dbfa0ffc5e781ae4
Viktor Velychko <Cezn.Vi@gmail.com>
2023-06-28 23:11:18 +0300

    W  I  P

--- SysProg__EXAM/Program.cs
+++ SysProg__EXAM/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System.Diagnostics;
 using System.Security;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace SysProg__EXAM
 {
@@ -9,7 +11,9 @@ namespace SysProg__EXAM
         /// <summary>
         /// Максимальная длина пароля
         /// </summary>
-        private const int maxLenghtPass = 5;
+        private const int maxLenghtPass = 4;
+
+        private const int minLenghtPass = 3;
 
         /// <summary>
         /// Переменная для хранения пароля который успешно подобран
@@ -19,32 +23,51 @@ namespace SysProg__EXAM
         /// <summary>
         /// Строка допустимых символов которые могут содержаться в пароле
         /// </summary>
-        private const string allPossibleSymbol = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*_";
+        private const string allPossibleSymbol = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*_";
 
 
         private static void SravniPass(string password) 
         {
-            if (password == "156q")
-                realPassword = password;
-            else
-                return;
+                if (password == "156q")
+                    realPassword = password;
+                else
+                    return;
         }
 
 
         static void Main(string[] args)
         {
-            //string pass = "";
-            //TryCheckPassword(pass);
 
-            Stopwatch sw = new Stopwatch();
-            sw.Start();
-            GeneratePass();
-            sw.Stop();
-            
+            //Stopwatch sw = new Stopwatch();
+            //sw.Start();
+            //GeneratePass();
+            //sw.Stop();
 
-            Console.WriteLine($"Real password is: {realPassword}");
-            Console.WriteLine($"Need time for this {sw.Elapsed.TotalSeconds} sec.");
+            //Console.ForegroundColor = ConsoleColor.Green;
+            //Console.WriteLine("Выполнение опараций АСИНХРОННО");
+            //Console.WriteLine($"Реальный пароль: {realPassword}");
+            //Console.WriteLine($"Затрачено времени {sw.Elapsed.TotalSeconds} сек.");
+            //Console.ResetColor();
+
+
+            //realPassword = "";
+            //Console.WriteLine();
+
+            Stopwatch sw2 = new Stopwatch();
+            sw2.Start();
+            GeneratePassSync();
+            sw2.Stop();
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Выполнение опараций СИНХРОННО");
+            Console.WriteLine($"Реальный пароль: {realPassword}");
+            Console.WriteLine($"Затрачено времени {sw2.Elapsed.TotalSeconds} сек.");
+            Console.ResetColor();
+
+
+
         }
+
 
 
         /// <summary>
@@ -72,8 +95,8 @@ namespace SysProg__EXAM
                 #pragma warning restore CA1416 // Отключение предупреждения
 
                 process.StartInfo.UseShellExecute = false;
-                process.Start();
 
+                process.Start();
                 realPassword = pass.ToString();
             }
             catch (Exception ex)
@@ -87,6 +110,7 @@ namespace SysProg__EXAM
         public static void GeneratePass()
         {
             List<Task> tasks = new List<Task>();
+            
 
             for (int i = 0; i < allPossibleSymbol.Length; i++)
             {
@@ -96,24 +120,87 @@ namespace SysProg__EXAM
                     Task.Run(() => { PassGenerateOneMore(pass1generation); })
                     );
 
+                ////tasks.Add(Task.Factory.StartNew(() => PassGenerateOneMore(pass1generation)));
             }
-
             Task.WaitAll(tasks.ToArray());
         }
+
+        private static void GeneratePassSync()
+        {
+            List<Task> tasks = new List<Task>();
+
+
+            for (int i = 0; i < allPossibleSymbol.Length; i++)
+            {
+                string pass1generation = allPossibleSymbol[i].ToString();
+
+                Task t = new Task(() => PassGenerateOneMore(pass1generation));
+                tasks.Add(t);
+                t.RunSynchronously();
+
+            }
+            Task.WaitAll(tasks.ToArray());
+        }
+
+
 
         /// <summary>
         /// Метод который порождает генарацию потоков и осуществляет подбор пароля
         /// </summary>
         /// <param name="pass"></param>
-        public async static void PassGenerateOneMore(object pass)
+        //public async static void PassGenerateOneMore(object pass)
+        //{
+        //    if (realPassword.Length > 0 || pass.ToString().Length > maxLenghtPass)
+        //        return;
+
+        //    //Console.WriteLine(pass);
+
+        //    //TryCheckPassword(pass);
+
+        //    //SravniPass(pass.ToString());
+
+        //    ///"Ручной тормоз" если пароль подобран то остановить порождение потоков
+        //    if (realPassword.Length > 0)
+        //        return;
+
+        //    //// "ручной тормоз" останавливает подбор паролей в случае привышения МАКС. длины пароля
+        //    if (pass.ToString().Length >= maxLenghtPass)
+        //        return;
+
+        //    for (int i = 0; i < allPossibleSymbol.Length; i++)
+        //    {
+        //        string passNextDepth = pass.ToString() + allPossibleSymbol[i].ToString();
+
+
+        //        if (passNextDepth.Length >= minLenghtPass)
+        //            SravniPass(passNextDepth);
+
+        //        //if (passNextDepth.Length >= minLenghtPass)
+        //        //    TryCheckPassword(passNextDepth);
+
+
+        //        ///"Ручной тормоз" если пароль подобран то остановить порождение потоков
+        //        if (realPassword.Length < 0 || passNextDepth.Length <= maxLenghtPass)
+        //            await Task.Run(() => PassGenerateOneMore(passNextDepth));
+        //        else
+        //            break;
+
+        //    }
+
+        //}
+
+
+
+        public static void PassGenerateOneMore(object pass)
         {
-            if (realPassword.Length > 0 || pass.ToString().Length >= maxLenghtPass)
+
+            if (realPassword.Length > 0 || pass.ToString().Length > maxLenghtPass)
                 return;
 
             //Console.WriteLine(pass);
 
             TryCheckPassword(pass);
-            
+
             //SravniPass(pass.ToString());
 
             ///"Ручной тормоз" если пароль подобран то остановить порождение потоков
@@ -121,29 +208,91 @@ namespace SysProg__EXAM
             //    return;
 
             //// "ручной тормоз" останавливает подбор паролей в случае привышения МАКС. длины пароля
-            //if (pass.ToString().Length >= maxLenghtPass)
-            //    return;
+            if (pass.ToString().Length >= maxLenghtPass)
+                return;
 
-            for(int i = 0;i < allPossibleSymbol.Length;i++)
+            List<Task> _activeTaskList = new List<Task>();
+            
+
+            for (int i = 0; i < allPossibleSymbol.Length; i++)
             {
                 string passNextDepth = pass.ToString() + allPossibleSymbol[i].ToString();
 
-                //SravniPass(passNextDepth);
-                TryCheckPassword(passNextDepth);
+                //if (passNextDepth.Length >= minLenghtPass)
+                //    SravniPass(passNextDepth);
 
+                if (passNextDepth.Length >= minLenghtPass)
+                    TryCheckPassword(passNextDepth);
 
                 ///"Ручной тормоз" если пароль подобран то остановить порождение потоков
-                if (realPassword.Length < 0 || passNextDepth.Length < maxLenghtPass)
-                    await Task.Run(() => PassGenerateOneMore(passNextDepth));
-                else
-                    break;
-            }
-            
+                if (realPassword.Length < 0 || passNextDepth.Length <= maxLenghtPass)
+                {
+                    Task task = Task.Factory.StartNew(() => PassGenerateOneMore(passNextDepth));
 
+                    _activeTaskList.Add(task);
+
+                }
+
+
+            }
+            if (realPassword.Length > 0)
+                return;
+
+            Task.WaitAll(_activeTaskList.ToArray());
 
         }
 
+        public static void PassGenerateOneMoreSync(object pass)
+        {
 
+            if (realPassword.Length > 0 || pass.ToString().Length > maxLenghtPass)
+                return;
+
+            //Console.WriteLine(pass);
+
+            TryCheckPassword(pass);
+
+            //SravniPass(pass.ToString());
+
+            ///"Ручной тормоз" если пароль подобран то остановить порождение потоков
+            //if (realPassword.Length > 0)
+            //    return;
+
+            //// "ручной тормоз" останавливает подбор паролей в случае привышения МАКС. длины пароля
+            if (pass.ToString().Length >= maxLenghtPass)
+                return;
+
+            List<Task> _activeTaskList = new List<Task>();
+
+
+            for (int i = 0; i < allPossibleSymbol.Length; i++)
+            {
+                string passNextDepth = pass.ToString() + allPossibleSymbol[i].ToString();
+
+                //if (passNextDepth.Length >= minLenghtPass)
+                //    SravniPass(passNextDepth);
+
+                if (passNextDepth.Length >= minLenghtPass)
+                    TryCheckPassword(passNextDepth);
+
+                ///"Ручной тормоз" если пароль подобран то остановить порождение потоков
+                if (realPassword.Length < 0 || passNextDepth.Length <= maxLenghtPass)
+                {
+                    Task task = new Task( () => PassGenerateOneMoreSync(passNextDepth));
+
+                    task.RunSynchronously();
+                    _activeTaskList.Add(task);
+
+                }
+
+
+            }
+            if (realPassword.Length > 0)
+                return;
+
+            Task.WaitAll(_activeTaskList.ToArray());
+
+        }
 
     }
 }
